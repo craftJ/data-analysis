8896d86b1ad241429c6bb7e8777540033f724f02
JohnDoe <johndoe@example.com>
2025-03-14 15:10:18 +0800

    [CHG][PASS]增加正弦demo，术语及部分参考信息

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # data-analysis
+
+## Terms
+- 虚拟变量（也称为独热编码，One-Hot Encoding）
+    >一种将分类变量转换为数值变量的方法。每个分类变量的类别（category）会被转换为一个独立的列，列中只有有 0 和 1 两种值，表示该类别是否出现
+
+- 多重共线性（Multicollinearity）
+    >统计学和机器学习中一个重要的概念，指的是在回归模型中，两个或多个解释变量（自变量）之间存在高度相关性。这种相关性可能导致模型的估计结果不稳定，参数解释不准确，甚至影响模型的预测性能。
+
+- VIF
+    >检测多重共线性的常用指标。VIF 值越高，表示该变量与其他变量之间的共线性越强。
+
+
+
+## Reference
+[sql join](https://learnsql.com/blog/sql-join-cheat-sheet/joins-cheat-sheet-a4.pdf)
+[统计学讲义](https://www.math.pku.edu.cn/teachers/lidf/course/probstathsy/probstathsy.pdf)
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,37 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+from pandas import Series,DataFrame
+
+def test():
+    #单层索引
+    sa = pd.Series([1, 2, 3], index=list('abc'))
+    print(sa)
+
+
+def draw_histogram():
+    
+
+def draw_curve():
+    # 计算正弦曲线上点的 x 和 y 坐标
+    x = np.arange(0,  3  * np.pi,  0.1) 
+    y = np.sin(x)
+    plt.title("sine wave form")
+
+    # 使用 matplotlib 来绘制点
+    plt.plot(x, y) 
+    plt.show()
+
+def main():
+    #draw_curve()
+    test()
+    return
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
 

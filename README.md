5a43edd8fdc54609d1fac0e1789aa5f236619408
craftJ <whiteday_dreamer@163.com>
2025-03-14 16:36:26 +0800

    [CHG][PASS]1.柱状图直方图demo及差异比较；2.增加调试接口分析调用堆栈; 3. 增加图表定期自动关闭

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # data-analysis
 
+## Graph
+- 直方图(histogram)
+    >适用于描述连续数据的集中趋势和离散程度,主要用来展示数据的分布情况,每个柱子表示一个区间内的数据频率
+- 柱状图(bar)
+    >适用于展示分类数据的计数、频率或其他度量,主要用来做差异比较，不同类别之间的数值大小
+
 ## Terms
 - 虚拟变量（也称为独热编码，One-Hot Encoding）
     >一种将分类变量转换为数值变量的方法。每个分类变量的类别（category）会被转换为一个独立的列，列中只有有 0 和 1 两种值，表示该类别是否出现
@@ -13,8 +19,8 @@
 
 
 ## Reference
-[sql join](https://learnsql.com/blog/sql-join-cheat-sheet/joins-cheat-sheet-a4.pdf)
-[统计学讲义](https://www.math.pku.edu.cn/teachers/lidf/course/probstathsy/probstathsy.pdf)
+- [sql join](https://learnsql.com/blog/sql-join-cheat-sheet/joins-cheat-sheet-a4.pdf)
+- [统计学讲义](https://www.math.pku.edu.cn/teachers/lidf/course/probstathsy/probstathsy.pdf)
 
 
 
--- utils.py
+++ utils.py
@@ -2,12 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 from pandas import Series,DataFrame
+import inspect
+import sys
+
+def dbg_caller():
+    # 获取调用者的帧信息
+    frame = sys._getframe(1)  # 1 表示调用栈的上一层
+    caller_line_number = frame.f_lineno
+    caller_function_name = frame.f_code.co_name
+    print(f"func:{caller_function_name}, line:{caller_line_number}")
+    return
 
 def test():
     #单层索引
     sa = pd.Series([1, 2, 3], index=list('abc'))
     print(sa)
 
+def show(plt,secs):
+    plt.show(block=False)
+    plt.pause(secs)
+    plt.close()
+    return
 
 #直方图
 def draw_histogram():
@@ -15,7 +30,28 @@ def draw_histogram():
     data = pd.Series(np.random.randn(17),index=list('abcdefghijklmnopq'))
     data.plot(kind='bar',ax=axes[0],color='red',alpha=0.7)
     data.plot(kind='barh',ax=axes[1],color='blue',alpha=0.7)
-    plt.show()
+    show(plt,1)
+    return
+
+#直方图
+def draw_hist():
+    data = np.random.randn(1000)  # 生成随机数据
+    plt.hist(data, bins=30, color='blue', alpha=0.7)
+    plt.title("Histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    show(plt,1)
+    return
+
+#柱状图
+def draw_bar():
+    categories = ['A', 'B', 'C', 'D']
+    values = [10, 15, 7, 5]
+    plt.bar(categories, values, color='green', alpha=0.7)
+    plt.title("Bar Chart")
+    plt.xlabel("Category")
+    plt.ylabel("Value")
+    show(plt,1)
     return
 
 #函数曲线
@@ -25,11 +61,14 @@ def draw_curve():
     y = np.sin(x)
     plt.title("y=sin(x)")
     plt.plot(x, y) 
-    plt.show()
+    show(plt, 1)
+    return
 
 def main():
     #draw_curve()
     draw_histogram()
+    draw_hist()
+    draw_bar()
     #test()
     return
 

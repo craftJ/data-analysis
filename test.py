fb142186f3eae28ca79c86e2562222b0027f7d6d
craftJ <johndoe@example.com>
2025-03-14 15:33:43 +0800

    [CHG][PASS]柱状图demo

--- test.py
+++ test.py
@@ -9,26 +9,33 @@ def test():
     print(sa)
 
 
+#直方图
 def draw_histogram():
-    
+    fig,axes = plt.subplots(2,1)
+    data = pd.Series(np.random.randn(17),index=list('abcdefghijklmnopq'))
+    data.plot(kind='bar',ax=axes[0],color='k',alpha=0.7)
+    data.plot(kind='barh',ax=axes[1],color='k',alpha=0.7)
+    plt.show()
+    return
 
+#函数曲线
 def draw_curve():
     # 计算正弦曲线上点的 x 和 y 坐标
     x = np.arange(0,  3  * np.pi,  0.1) 
     y = np.sin(x)
-    plt.title("sine wave form")
-
-    # 使用 matplotlib 来绘制点
+    plt.title("y=sin(x)")
     plt.plot(x, y) 
     plt.show()
 
 def main():
     #draw_curve()
-    test()
+    draw_histogram()
+    #test()
     return
 
 if __name__ == '__main__':
     main()
+
 
 
 

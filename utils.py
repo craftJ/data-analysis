0d6d9c0a4ffd5f415cad56f8bcf56799293b66f9
craftJ <whiteday_dreamer@163.com>
2025-03-15 23:11:31 +0800

    [CHG][PASS]新增螺旋线demo

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from pandas import Series,DataFrame
 import inspect
 import sys
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
 def dbg_caller():
     # 获取调用者的帧信息
@@ -89,21 +90,47 @@ def draw_bar():
     return
 
 #函数曲线
-def draw_curve():
-    # 计算正弦曲线上点的 x 和 y 坐标
-    x = np.arange(0,  3  * np.pi,  0.1) 
-    y = np.sin(x)
-    plt.title("y=sin(x)")
-    plt.plot(x, y) 
-    show(plt, 1)
+def draw_curve(funtype):
+    match funtype:
+        case "sin":
+            # 计算正弦曲线上点的 x 和 y 坐标
+            x = np.arange(0,  4 * np.pi,  0.1) 
+            y = np.sin(x)
+            plt.title("y=sin(x)")
+            plt.plot(x, y) 
+            show(plt, 1)
+        case "helix":
+            # 定义参数
+            a = 1  # 半径
+            b = 0.2  # 螺距
+            t = np.linspace(0, 8 * np.pi, 100)  # 参数 t 的取值范围
+
+            # 计算螺旋线的坐标
+            x = a * np.cos(t)
+            y = a * np.sin(t)
+            z = b * t
+
+            # 绘制螺旋线
+            fig = plt.figure(figsize=(10, 10))
+            ax = fig.add_subplot(111,projection='3d')
+            ax.plot(x, y, z)
+
+            # 设置坐标轴标签
+            plt.title("helix")
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            show(plt)
+        case _:
+            print("Unknown function!!")
     return
 
 def main():
-    #draw_curve()
+    draw_curve("helix")
     #draw_histogram()
     #draw_hist()
     #draw_bar()
-    draw_heatmap()
+    #draw_heatmap()
     #test()
     return
 

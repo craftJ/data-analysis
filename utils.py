2ea579e5ac60d65d56d9707a85721eb5aff8446d
craftJ <whiteday_dreamer@163.com>
2025-03-17 09:31:47 +0800

    [CHG][PASS]解决match-case在3.10版本下不支持的兼容性问题

--- utils.py
+++ utils.py
@@ -91,38 +91,37 @@ def draw_bar():
 
 #函数曲线
 def draw_curve(funtype):
-    match funtype:
-        case "sin":
-            # 计算正弦曲线上点的 x 和 y 坐标
-            x = np.arange(0,  4 * np.pi,  0.1) 
-            y = np.sin(x)
-            plt.title("y=sin(x)")
-            plt.plot(x, y) 
-            show(plt, 1)
-        case "helix":
-            # 定义参数
-            a = 1  # 半径
-            b = 0.2  # 螺距
-            t = np.linspace(0, 8 * np.pi, 100)  # 参数 t 的取值范围
+    if funtype == "sin":
+        # 计算正弦曲线上点的 x 和 y 坐标
+        x = np.arange(0,  4 * np.pi,  0.1) 
+        y = np.sin(x)
+        plt.title("y=sin(x)")
+        plt.plot(x, y) 
+        show(plt, 1)
+    elif funtype == "helix":
+        # 定义参数
+        a = 1  # 半径
+        b = 0.2  # 螺距
+        t = np.linspace(0, 8 * np.pi, 100)  # 参数 t 的取值范围
 
-            # 计算螺旋线的坐标
-            x = a * np.cos(t)
-            y = a * np.sin(t)
-            z = b * t
+        # 计算螺旋线的坐标
+        x = a * np.cos(t)
+        y = a * np.sin(t)
+        z = b * t
 
-            # 绘制螺旋线
-            fig = plt.figure(figsize=(10, 10))
-            ax = fig.add_subplot(111,projection='3d')
-            ax.plot(x, y, z)
+        # 绘制螺旋线
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111,projection='3d')
+        ax.plot(x, y, z)
 
-            # 设置坐标轴标签
-            plt.title("helix")
-            ax.set_xlabel('X')
-            ax.set_ylabel('Y')
-            ax.set_zlabel('Z')
-            show(plt)
-        case _:
-            print("Unknown function!!")
+        # 设置坐标轴标签
+        plt.title("helix")
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        show(plt)
+    else:
+        print("Unknown function!!")
     return
 
 def main():
